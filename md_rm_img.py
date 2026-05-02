1625481a9badbdaae5f281115314b48144036965
tintinly <821294434@qq.com>
2026-05-02 14:08:59 +0800

    避免影响asset中非图像文件

--- md_rm_img.py
+++ md_rm_img.py
@@ -2,7 +2,7 @@ import re
 import os 
 import glob
 import shutil
- 
+
 # 该脚本功能：清理 Typora markdown笔记中未被引用的图片
 # 1.递归遍历目录中的所有md文档，获取md文档中被引用的图片地址
 # 2.获取md文档所在目录下的assets文件夹中所有图片地址
@@ -20,8 +20,8 @@ ASSETS_NAME = "assets"
  
 # 定义一个正则表达式，匹配md文档中被引用的图片地址
 # 这个很重要，尽可能地匹配到所有typora的图片引用，否则匹配不全将导致很多图片误判为未被引用，导致误删或误移动
-pattern = r'\!\[[^\]]*\]\(([^)]*)\)'
-pattern2 = r'<img\s+[^>]*src=["\']([^"\']*)["\'][^>]*/?>'
+pattern = r'\!\[[^\]]*\]\(([^)]*)\)' # 形如 ![alt text][path]
+pattern2 = r'<img\s+[^>]*src=["\']([^"\']*)["\'][^>]*/?>' # 形如 <img src="path">
 
 # 存储图片地址的提醒
 warn_total_list = []
@@ -40,12 +40,14 @@ for md_path in glob.glob(os.path.join(ROOT_DIR, "**", "*.md"), recursive=True):
     text=""
     warn_list=[]
     # 获取存储这个md文档图片文件夹下所有图片名称
+    img_suffix = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp', '.svg')
     assets_dir = os.path.join(md_dir, ASSETS_NAME)
     for asset_path in glob.glob(os.path.join(assets_dir, '*')):
-        # print(asset_path)
-        format_asset_path = os.path.join(ASSETS_NAME, os.path.basename(asset_path))
-        # print(format_asset_path)
-        asset_set.add(format_asset_path)
+        if asset_path.lower().endswith(img_suffix):
+            # print(asset_path)
+            format_asset_path = os.path.join(ASSETS_NAME, os.path.basename(asset_path))
+            # print(format_asset_path)
+            asset_set.add(format_asset_path)
 	# 读取文件 匹配获取图片引用
     with open(md_path,"r",encoding="utf-8") as f:
         text+=f.read()
@@ -72,9 +74,9 @@ for md_path in glob.glob(os.path.join(ROOT_DIR, "**", "*.md"), recursive=True):
         old_asset = os.path.join(md_dir, abandon_asset)
         new_asset = os.path.join(ROOT_DIR, "未被引用的图片", md_name.split('.')[0] + "_" + abandon_asset)
         new_dir = os.path.dirname(new_asset)
-        if not os.path.exists(new_dir):
-            os.makedirs(new_dir)
-        shutil.move(old_asset, new_asset)
+        # if not os.path.exists(new_dir):
+        #     os.makedirs(new_dir)
+        # shutil.move(old_asset, new_asset)
         warn_list.append("未被引用的图片," + old_asset + "\n移动至," + new_asset)
     for non_img in (img_set-asset_set):
         warn_list.append("无效的图片地址,"+ img_dict[non_img])
